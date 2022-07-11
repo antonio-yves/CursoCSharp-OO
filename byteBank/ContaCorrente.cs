ed6fcc4a7dc51cbf8740369b7aad8cbcdcc84425
Antonio Yves <yvissousa@gmail.com>
2022-07-11 10:24:05 -0300

    Adicionada a classe ContaCorrente

---
+++ byteBank/ContaCorrente.cs
@@ -0,0 +1,11 @@
+﻿namespace byteBank
+{
+    public class ContaCorrente
+    {
+        public string Titular;
+        public string Conta;
+        public int Agencia;
+        public string NomeAgencia;
+        public double Saldo;
+    }
+}
--- byteBank/Program.cs
+++ byteBank/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] agrs)
+    {
+        Console.WriteLine("Bem vindx ao byteBank!");
+
+        string NomeTitular = "João Gomes";
+        string Conta = "1234-x";
+        int Agencia = 23;
+        string NomeAgencia = "Agência Central";
+        double Saldo = 93.50;
+
+        Console.WriteLine("\nNome do Titular: " + NomeTitular);
+        Console.WriteLine("Agência: " + Agencia);
+        Console.WriteLine("Conta: " + Conta);
+        Console.WriteLine("Nome da Agência: " + NomeAgencia);
+        Console.WriteLine("Saldo: R$ " + Saldo);
+    }
+}
