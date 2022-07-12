57583fec835417ce3671efd1e1f47e2e602397a5
Antonio Yves <yvissousa@gmail.com>
2022-07-11 21:03:48 -0300

    Adicionado os métodos: Sacar, Depositar, Transferir e ExibirDadosDaConta

--- byteBank/ContaCorrente.cs
+++ byteBank/ContaCorrente.cs
@@ -7,5 +7,49 @@
         public int Agencia;
         public string NomeAgencia;
         public double Saldo;
+
+        public bool Sacar(double valor)
+        {
+            if ((Saldo < valor) || (valor < 0))
+            {
+                return false;
+            }
+            else
+            {
+                Saldo -= valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            if (valor > 0)
+            {
+                Saldo += valor;
+            }
+        }
+
+        public bool Transferir(double valor, ContaCorrente destino)
+        {
+            if ((Saldo < valor) || (valor < 0))
+            {
+                return false;
+            }
+            else
+            {
+                Saldo -= valor;
+                destino.Saldo += valor;
+                return true;
+            }
+        }
+
+        public void ExibirDadosDaConta()
+        {
+            Console.WriteLine("Titular: " + Titular);
+            Console.WriteLine("Agência: " + Agencia);
+            Console.WriteLine("Conta: " + Conta);
+            Console.WriteLine("Nome da Agência: " + NomeAgencia);
+            Console.WriteLine("Saldo: R$ " + Saldo);
+        }
     }
 }
